6bb5d5c0155f0a390bdc5319020473588d2e2e84
TNimsara <mktharushi2000@gmail.com>
2024-10-19 10:45:43 +0530

    Sidebar component

---
+++ csProject/resources/js/Pages/Customer/Sidebar.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from '@inertiajs/react';
+import './Sidebar.css';
+
+export default function Sidebar() {
+    return (
+        <div className="sidebar">
+            <ul>
+                <li>
+                    <Link href="/dashboard" className="nav-link">Dashboard</Link>
+                </li>
+                <li>
+                    <Link href="/vehicles" className="nav-link">My Vehicles</Link>
+                </li>
+                <li>
+                    <Link href="/appointments" className="nav-link">Appointments</Link>
+                </li>
+                <li>
+                    <Link href="/service-history" className="nav-link">Service History</Link>
+                </li>
+                <li>
+                    <Link href="/offers" className="nav-link">Service Packages/Offers</Link>
+                </li>
+                <li>
+                    <Link href="/reminders" className="nav-link">Reminders</Link>
+                </li>
+                <li>
+                    <Link href="/billing" className="nav-link">Billing & Payments</Link>
+                </li>
+                <li>
+                    <Link href="/support" className="nav-link">Support</Link>
+                </li>
+                <li>
+                    <Link href="/settings" className="nav-link">Settings</Link>
+                </li>
+                <li>
+                    <Link href="/logout" className="nav-link">Logout</Link>
+                </li>
+            </ul>
+        </div>
+    );
+}
