de4cb8d7e78db256a277cc18581f67ab9a5a201e
TNimsara <mktharushi2000@gmail.com>
2024-10-19 18:41:19 +0530

    Create 2024_10_19_130852_create_business_hours_table.php

---
+++ csProject/database/migrations/2024_10_19_130852_create_business_hours_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('business_hours', function (Blueprint $table) {
+            $table->id();
+            $table->enum('dayOfWeek', ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'])->notNullable();
+            $table->time('openingTime')->notNullable();
+            $table->time('closingTime')->notNullable();
+            $table->boolean('isOpen')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('business_hours');
+    }
+};
