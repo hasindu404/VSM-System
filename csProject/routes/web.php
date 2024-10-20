2b3155e16c47b6f5be41aa8bfe968a10643a1fc0
TNimsara <mktharushi2000@gmail.com>
2024-10-20 14:57:28 +0530

    Update web.php

--- csProject/routes/web.php
+++ csProject/routes/web.php
@@ -29,6 +29,10 @@ Route::get('/CustomerDashboard', function () {
     return Inertia::render('Customer/CustomerDashboard'); // Ensure the casing matches
 })->name('CustomerDashboard');
 
+Route::get('/AdminDashboard', function () {
+    return Inertia::render('Admin/AdminDashboard'); // Ensure the casing matches
+})->name('AdminDashboard');
+
 Route::get('/create', function () {
     return Inertia::render('Appointments/Create'); // Ensure the casing matches
 })->name('create');
