a93e8a6ac4b8af3ec3494717606ebfa932f3b4ce
AchinthaDev <achinthaisurusara2020@gmail.com>
2024-10-22 11:12:00 +0530

    vehical_blade

--- csProject/routes/web.php
+++ csProject/routes/web.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\VehicalController;
 use App\Http\Controllers\BusinessHourController;
+use App\Models\Vehical;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -26,6 +28,12 @@ Route::post('/custmang',[CustomerController::class, 'store'])->name('customer.st
 Route::get('/custmang/{customer}/edit',[CustomerController::class, 'edit'])->name('customer.edit');
 Route::put('/custmang/{customer}/update',[CustomerController::class, 'update'])->name('customer.update');
 Route::delete('/custmang/{customer}/delete',[CustomerController::class, 'delete'])->name('customer.delete');
+
+Route::get('/vehimang',[VehicalController::class,'index'])->name('vehical.vehimang');
+Route::post('/vehimang',[VehicalController::class, 'store'])->name('vehical.store');
+Route::get('/vehimang/{vehical}/edit',[VehicalController::class, 'edit'])->name('vehical.edit');
+Route::put('/vehimang/{vehical}/update',[VehicalController::class, 'update'])->name('vehical.update');
+Route::delete('/vehimang/{vehical}/delete',[VehicalController::class, 'delete'])->name('vehical.delete');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
