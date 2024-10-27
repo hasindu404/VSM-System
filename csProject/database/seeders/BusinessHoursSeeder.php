0dd0bfdac0fdf47ed41ee0cf211d1bbbe664f86a
TNimsara <mktharushi2000@gmail.com>
2024-10-27 15:47:59 +0530

    Update BusinessHoursSeeder.php

--- csProject/database/seeders/BusinessHoursSeeder.php
+++ csProject/database/seeders/BusinessHoursSeeder.php
@@ -12,20 +12,24 @@ class BusinessHoursSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    {
-        $businessHours = [
-            ['dayOfWeek' => 'Monday', 'openingTime' => '09:00', 'closingTime' => '17:00', 'isOpen' => true],
-            ['dayOfWeek' => 'Tuesday', 'openingTime' => '09:00', 'closingTime' => '17:00', 'isOpen' => true],
-            ['dayOfWeek' => 'Wednesday', 'openingTime' => '09:00', 'closingTime' => '17:00', 'isOpen' => true],
-            ['dayOfWeek' => 'Thursday', 'openingTime' => '09:00', 'closingTime' => '17:00', 'isOpen' => true],
-            ['dayOfWeek' => 'Friday', 'openingTime' => '09:00', 'closingTime' => '17:00', 'isOpen' => true],
-            ['dayOfWeek' => 'Saturday', 'openingTime' => '10:00', 'closingTime' => '14:00', 'isOpen' => true],
-            ['dayOfWeek' => 'Sunday', 'openingTime' => null, 'closingTime' => null, 'isOpen' => false], // Closed on Sunday
-            
-        ];
+{
+    $businessHours = [
+        ['dayOfWeek' => 'Monday', 'openingTime' => '09:00', 'closingTime' => '17:00', 'isOpen' => true, 'step' => 30],
+        ['dayOfWeek' => 'Tuesday', 'openingTime' => '09:00', 'closingTime' => '17:00', 'isOpen' => true, 'step' => 30],
+        ['dayOfWeek' => 'Wednesday', 'openingTime' => '09:00', 'closingTime' => '17:00', 'isOpen' => true, 'step' => 30],
+        ['dayOfWeek' => 'Thursday', 'openingTime' => '09:00', 'closingTime' => '17:00', 'isOpen' => true, 'step' => 30],
+        ['dayOfWeek' => 'Friday', 'openingTime' => '09:00', 'closingTime' => '17:00', 'isOpen' => true, 'step' => 30],
+        ['dayOfWeek' => 'Saturday', 'openingTime' => '10:00', 'closingTime' => '14:00', 'isOpen' => true, 'step' => 60],
+        ['dayOfWeek' => 'Sunday', 'openingTime' => null, 'closingTime' => null, 'isOpen' => false, 'step' => null], // Closed on Sunday
+    ];
 
-        foreach ($businessHours as $hours) {
-            BusinessHour::create($hours);
-        }
+    foreach ($businessHours as $hours) {
+        // Use updateOrCreate to avoid duplicate entries
+        BusinessHour::updateOrCreate(
+            ['dayOfWeek' => $hours['dayOfWeek']], // Condition to find the record
+            $hours // Data to be updated or created
+        );
     }
+}
+
 }
