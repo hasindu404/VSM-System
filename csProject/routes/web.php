a88e78f000d138014865ed59140bd5abc1338046
TNimsara <mktharushi2000@gmail.com>
2024-11-01 21:27:52 +0530

    Update web.php

--- csProject/routes/web.php
+++ csProject/routes/web.php
@@ -116,6 +116,10 @@ Route::post('/appointments', [AppointmentController::class, 'store'])->name('app
 Route::put('/business-hours/update/{dayOfWeek}', [BusinessHourController::class, 'update'])->name('business.hours.update');
 Route::get('/booked-times/{date}', [AppointmentController::class, 'getBookedTimes'])->name('booked-times');
 Route::get('/business-hours/{dayOfWeek}', [BusinessHourController::class, 'show']);
+Route::get('/closed-days', [BusinessHourController::class, 'getClosedDays'])->name('closed-days');
+
+
+
 
 //feedback
 Route::get('/feedbacksub',function(){
